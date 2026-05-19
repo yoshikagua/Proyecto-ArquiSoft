7f1c0887cc448a42052370acb4cffd43fd8876aa
dcamelos <dcamelos@unal.edu.com>
2026-05-18 23:49:16 -0500

    add payment api to reverse proxy

--- mf_payments/next.config.mjs
+++ mf_payments/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  assetPrefix: '/donations',
+  // ⚠️ ESTO ES LO CRUCIAL:
+  // Hace que Next.js maneje internamente todas sus páginas bajo /donations
+  basePath: '/donations',
+  
+  // Si tenías assetPrefix, puedes dejarlo o quitarlo, basePath suele encargarse de ambos en Next moderno
+  assetPrefix: '/donations', 
 };
 
 export default nextConfig;
